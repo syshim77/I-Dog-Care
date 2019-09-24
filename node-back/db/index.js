455fff77bee2a4893fdcde764a5469c7970c6aa3
sooyeon Shim <syshim77@sooyeonui-MacBook-Pro.local>
2019-09-24 21:28:48 +0900

    modify db index.js code

--- node-back/db/index.js
+++ node-back/db/index.js
@@ -26,14 +26,25 @@ app.get('/action/time', function(req, res){
     connection.query('SELECT time from action_table_g', function(err, rows) {
         if(err) throw err;
 
-        console.log('The solution is: ', rows, '\n');
-        console.log('The solution is for rows[0]: ', rows[0], '\n');
+        // console.log('The solution is: ', rows, '\n');
+        // console.log('The solution is for rows[0]: ', rows[0], '\n');
         // var dateStr = JSON.parse(rows[1]);
         // var dateStr = JSON.parse(rows);
         // var date = new Date(dateStr);
         // var date = new Date(rows[1]);
-        console.log('rows.toString(): ', rows.toString());
+        // console.log('rows.toString(): ', rows.toString());
         // console.log('The solution is: ', date, '\n');
+
+        for (var i=0; i<rows.length; i++) {
+            var row = rows[i];
+            var date = JSON.parse(row);
+            var dateTime = JSON.parse(row).time;
+            console.log('The solution is row: ', row, '\n');
+            console.log('JSON.parse(row): ', date, '\n');
+            console.log('JSON.parse(row).time: ', dateTime, '\n');
+            console.log('\n');
+        }
+
         res.send(rows);
     });
 });
