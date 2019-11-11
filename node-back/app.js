f286a989c45c2d7cd3993145327ea8e200b1d6b5
sooyeon Shim <syshim77@sooyeonui-MacBook-Pro.local>
2019-11-11 20:38:02 +0900

    modify back-end code per five minutes

--- node-back/app.js
+++ node-back/app.js
@@ -125,13 +125,46 @@ app.get('/day', (req, res) => {
   });
 });
 
+app.get('/activity/day', cors(), (req, res) => {
+  connection.query('SELECT action, outside from action_table_h', function(err, rows) {
+    if (err) throw err;
+
+    var actionOutside = [];
+    var actionDay = new Array(rows.length/288);
+    var outsideDay = new Array(rows.length/288);
+    var cnt=0;
+
+    for (var i=0; i<rows.length; i+=288) {
+      actionDay[cnt] = 0;
+      outsideDay[cnt] = 0;
+      for (var j=0;j<288; j++) {
+        // var row = rows[i+j];
+        // actionDay[cnt] += row.duration
+        actionDay[cnt] += rows[i+j].action;
+        outsideDay[cnt] += rows[i+j].outside;
+      }
+      cnt++;
+    }
+
+    actionOutside.push(actionDay, outsideDay);
+
+    res.send(JSON.stringify(actionOutside));
+  });
+});
+
 app.get('/activity/five', cors(), (req, res) => {
   connection.query('SELECT action from action_table_h', function(err, rows) {
     if (err) throw err;
 
     console.log('The solution is: ', rows, '\n');
 
-    var action = new Array(rows.length/6);
+    // var action = new Array(rows.length/6);
+    // var action48 = new Array(48);
+    // var action24 = new Array(24);
+
+    // var action = new Array();
+    var action = [];
+    // const action = [];
     var action48 = new Array(48);
     var cnt=0;
 
@@ -139,32 +172,84 @@ app.get('/activity/five', cors(), (req, res) => {
     //   console.log('The solution is: ', rows[i], '\n');
     // }
 
+
     for (var i=0; i<rows.length; i+=6) {
-      // var thirty = 0;
-      action[cnt]=0;
+      action48[cnt]=0;
+
       for (var j=0; j<6; j++) {
-        // thirty += rows[i+j];
-        action[cnt] += rows[i+j].action;
+        action48[cnt] += rows[i+j].action;
       }
-      // action[cnt] = thirty;
+      cnt++;
+
+      if (cnt === 48) {
+        action.push(action48);
+        // console.log(action48);
+        // console.log(action);
+        // console.log('\n');
+
+        action48 = new Array(48);
+        cnt=0;
+      }
+      // console.log(action);
+    }
+    // console.log(action);
+    res.send(JSON.stringify(action));
+    // res.send(JSON.stringify(action48));
+    // res.send(JSON.stringify(action24));
+  });
+});
+
+app.get('/sleep/day', cors(), (req, res) => {
+  connection.query('SELECT sleep from sleep_table_h', function(err, rows) {
+    if (err) throw err;
+
+    var sleepDay = new Array(rows.length/288);
+    var cnt = 0;
+
+    for (var i=0; i<rows.length; i+=288) {
+      sleepDay[cnt] = 0;
+      for (var j=0; j<288; j++) {
+        sleepDay[cnt] += rows[i+j].sleep;
+      }
       cnt++;
     }
 
-    for (var k=0; k<48; k++) {
-      action48[k] = 0;
-      for (var s=0; s<3; s++) {
-        action48[k] += rows[s].action;
-      }
-      // action48[k] = rows[k].action;
-      // console.log('The action48 is: ', action48[k], ', ');
-    }
-
-    console.log('The action48 is: ', action48);
-
-    // res.send(JSON.stringify(action));
-    res.send(JSON.stringify(action48));
-  });
+    res.send(JSON.stringify(sleepDay));
+  })
 });
+
+app.get('/sleep/five', cors(), (req, res) => {
+  connection.query('SELECT sleep from sleep_table_h', function(err, rows) {
+    if (err) throw err;
+
+    var sleep = [];
+    var sleep48 = new Array(48);
+    var cnt = 0;
+
+    for (var i=0; i<rows.length; i+=6) {
+      sleep48[cnt]=0;
+
+      for (var j=0; j<6; j++) {
+        sleep48[cnt] += rows[i+j].sleep;
+      }
+      cnt++;
+
+      if (cnt === 48) {
+        sleep.push(sleep48);
+        // console.log(action48);
+        // console.log(action);
+        // console.log('\n');
+
+        sleep48 = new Array(48);
+        cnt=0;
+      }
+      // console.log(action);
+    }
+    // console.log(action);
+    res.send(JSON.stringify(sleep));
+  })
+});
+
 
 // 산책 시간에 대한 table 추가되어야함
 app.get('/activity', cors(), (req, res) => {
@@ -284,28 +369,128 @@ app.get('/calorie', cors(), (req, res) => {
 });
 
 // average, max 두 개 어떻게 JSON으로 보낼건지 고민
-app.get('/temperature', cors(), (req, res) => {
+app.get('/temperature/day', cors(), (req, res) => {
   connection.query('SELECT average, max from temp_table_g', function(err, rows) {
     if(err) throw err;
 
     // console.log('The solution is: ', rows);
+    var temperature = [];
+    var average = new Array(rows.length/144);
+    var max = new Array(rows.length/144);
+    var cnt = 0;
+    //
+    for (var i=0; i<rows.length; i+=144) {
+      // var row1 = rows[i];
+      // var row2 = rows[i+1];
+      // var row3 = rows[i+2];
+      // duration.push(row.duration);
+
+      // average[cnt] = (rows[i].average + rows[i+1].average + rows[i+2].average)/3;
+      // max[cnt] = (rows[i].max + rows[i+1].max + rows[i+2].max)/3;
+      // cnt++;
+      average[cnt]=0; max[cnt]=0;
+      for (var j=0; j<144; j++) {
+        average[cnt] += rows[i+j].average;
+        max[cnt] += rows[i+j].max;
+        // max[cnt] = Math.max(max[cnt], rows[i+j].max); // 10분 단위 최대값 중 최대값
+      }
+
+      average[cnt] /= 144;
+      max[cnt] /= 144;  // 최대값들의 평균값
+      cnt++;
+      // console.log('The solution is average[', i, ']: ', average[i], ', max[', i, ']: ', max[i], '\n');
+    }
+
+    temperature.push(max, average);
+
+    // res.send(rows);
+    res.send(JSON.stringify(temperature));
+  });
+});
+
+app.get('/temperature/five', cors(), (req, res) => {
+  connection.query('SELECT average, max from temp_table_g', function(err, rows) {
+    if(err) throw err;
+
+    // console.log('The solution is: ', rows);
+    var temperature = [];
     var average = new Array(rows.length/3);
     var max = new Array(rows.length/3);
     var cnt = 0;
     //
     for (var i=0; i<rows.length; i+=3) {
-      var row1 = rows[i];
-      var row2 = rows[i+1];
-      var row3 = rows[i+2];
+      // var row1 = rows[i];
+      // var row2 = rows[i+1];
+      // var row3 = rows[i+2];
       // duration.push(row.duration);
-      average[cnt] = (row1.average + row2.average + row3.average)/3;
-      max[cnt] = (row1.max + row2.max + row3.max)/3;
+      average[cnt] = (rows[i].average + rows[i+1].average + rows[i+2].average)/3;
+      max[cnt] = (rows[i].max + rows[i+1].max + rows[i+2].max)/3;
       cnt++;
-      console.log('The solution is average[', i, ']: ', average[i], ', max[', i, ']: ', max[i], '\n');
+      // console.log('The solution is average[', i, ']: ', average[i], ', max[', i, ']: ', max[i], '\n');
     }
 
+    temperature.push(average, max);
+
     // res.send(rows);
-    res.send(JSON.stringify(duration));
+    res.send(JSON.stringify(temperature));
+  });
+});
+
+app.get('/hrv/day', cors(), (req, res) => {
+  connection.query('SELECT average, max from heart_table_g', function(err, rows) {
+    if(err) throw err;
+
+    // console.log('The solution is: ', rows);
+    var hrv = [];
+    var average = new Array(rows.length/144);
+    var max = new Array(rows.length/144);
+    var cnt = 0;
+    //
+    for (var i=0; i<rows.length; i+=144) {
+      average[cnt]=0; max[cnt]=0;
+      for (var j=0; j<144; j++) {
+        average[cnt] += rows[i+j].average;
+        max[cnt] += rows[i+j].max;
+      }
+
+      average[cnt] /= 144;
+      max[cnt] /= 144;
+      cnt++;
+    }
+
+    hrv.push(max, average);
+
+    // res.send(rows);
+    res.send(JSON.stringify(hrv));
+  });
+});
+
+app.get('/hrv/five', cors(), (req, res) => {
+  connection.query('SELECT average, max from heart_table_g', function(err, rows) {
+    if(err) throw err;
+
+    // console.log('The solution is: ', rows);
+    var hrv = [];
+    var average = new Array(rows.length/144);
+    var max = new Array(rows.length/144);
+    var cnt = 0;
+    //
+    for (var i=0; i<rows.length; i+=144) {
+      average[cnt]=0; max[cnt]=0;
+      for (var j=0; j<144; j++) {
+        average[cnt] += rows[i+j].average;
+        max[cnt] += rows[i+j].max;
+      }
+
+      average[cnt] /= 144;
+      max[cnt] /= 144;
+      cnt++;
+    }
+
+    hrv.push(max, average);
+
+    // res.send(rows);
+    res.send(JSON.stringify(hrv));
   });
 });
 
